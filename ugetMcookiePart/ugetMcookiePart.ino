cb360c4e5278cb63c517e9fd54cd792a6b37d000
hyhzxhy <32429436+hyhzxhy@users.noreply.github.com>
2017-12-20 21:44:20 +0800

    Add files via upload

---
+++ ugetMcookiePart/ugetMcookiePart.ino
@@ -0,0 +1,183 @@
+#define INTERVAL_SENSOR   17000             //定义传感器采样时间间隔  597000
+#define INTERVAL_NET      17000             //定义发送时间
+//传感器部分================================   
+#include <Wire.h>                                  //调用库  
+#include "./ESP8266.h"
+#include "I2Cdev.h"                                //调用库  
+//温湿度   
+#include <SHT2x.h>
+//光照
+#define mic_pin A1
+#define PIR_PIN 6
+#define SSID           "test"                   // cannot be longer than 32 characters!
+#define PASSWORD       "12345678"
+
+#define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
+                                   // received before closing the connection.  If you know the server
+                                   // you're accessing is quick to respond, you can reduce this value.
+
+//WEBSITE     
+#define HOST_NAME   "api.heclouds.com"  
+#define DEVICEID   "23343719"
+#define PROJECTID "102517"
+#define HOST_PORT   (80)
+String apiKey="EvldL=Gho20lXXcXT0r=dCRxznk=";
+char buf[20];
+
+#define INTERVAL_sensor 2000
+unsigned long sensorlastTime = millis();
+
+float tempOLED, humiOLED, lightnessOLED;
+
+#define INTERVAL_OLED 1000
+
+String mCottenData;
+String jsonToSend;
+
+//3,传感器值的设置 
+float sensor_tem, sensor_hum, sensor_voi,sensor_pir,db;                    //传感器温度、湿度、光照   
+char  sensor_tem_c[7], sensor_hum_c[7], sensor_voi_c[7],sensor_pir_c[7],db_c[7] ;    //换成char数组传输
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3); /* RX:D3, TX:D2 */
+ESP8266 wifi(mySerial);
+//ESP8266 wifi(Serial1);                                      //定义一个ESP8266（wifi）的对象
+unsigned long net_time1 = millis();                          //数据上传服务器时间
+unsigned long sensor_time = millis();                        //传感器采样时间计时器
+
+//int SensorData;                                   //用于存储传感器数据
+String postString;                                //用于存储发送数据的字符串
+//String jsonToSend;                                //用于存储发送的json格式参数
+
+void setup(void)     //初始化函数  
+{       
+  //初始化串口波特率  
+    Wire.begin();
+    Serial.begin(115200);   
+    while(!Serial);
+    pinMode(PIR_PIN, INPUT); 
+    pinMode(mic_pin, INPUT);
+
+   //ESP8266初始化
+    Serial.print("setup begin\r\n");   
+
+  Serial.print("FW Version:");
+  Serial.println(wifi.getVersion().c_str());
+
+  if (wifi.setOprToStationSoftAP()) {
+    Serial.print("to station + softap ok\r\n");
+  } else {
+    Serial.print("to station + softap err\r\n");
+  }
+
+  if (wifi.joinAP(SSID, PASSWORD)) {      //加入无线网
+    Serial.print("Join AP success\r\n");  
+    Serial.print("IP: ");
+    Serial.println(wifi.getLocalIP().c_str());
+  } else {
+    Serial.print("Join AP failure\r\n");
+  }
+
+  if (wifi.disableMUX()) {
+    Serial.print("single ok\r\n");
+  } else {
+    Serial.print("single err\r\n");
+  }
+
+  Serial.print("setup end\r\n");
+    
+  
+}
+void loop(void)     //循环函数  
+{   
+  if (sensor_time > millis())  sensor_time = millis();  
+    
+  if(millis() - sensor_time > INTERVAL_SENSOR)              //传感器采样时间间隔  
+  {  
+    getSensorData();                                        //读串口中的传感器数据
+    sensor_time = millis();
+  }  
+
+    
+  if (net_time1 > millis())  net_time1 = millis();
+  
+  if (millis() - net_time1 > INTERVAL_NET)                  //发送数据时间间隔
+  {                
+    updateSensorData();                                     //将数据上传到服务器的函数
+    net_time1 = millis();
+  }
+  
+}
+
+void getSensorData(){  
+  sensor_tem = SHT2x.readT() ; //读取温度  
+  sensor_hum = SHT2x.readRH();//读取相对湿度   
+  sensor_voi = analogRead(A0);//读取声音
+  float temp= sensor_voi;
+  sensor_voi=map(sensor_voi,0,1023,0,5);
+  db = (20*log(10)) * (sensor_voi/1.0);//此处是想将声音传感器的信号转化为分贝，但是由于声音传感器敏感度较低，真正实现的时候并没有用这个数据    
+  sensor_voi=temp;//重新使用声音传感器的原始数据
+  sensor_pir=digitalRead(PIR_PIN);
+  dtostrf(sensor_tem, 2, 1, sensor_tem_c);
+  dtostrf(sensor_hum, 2, 1, sensor_hum_c);
+  dtostrf(sensor_voi, 3, 1, sensor_voi_c);
+  dtostrf(db,3,1,db_c);
+  dtostrf( sensor_pir, 2, 1,  sensor_pir_c);//将数据转化为字符串，便于上传
+}
+void updateSensorData() {
+  if (wifi.createTCP(HOST_NAME, HOST_PORT)) { //建立TCP连接，如果失败，不能发送该数据
+    Serial.print("create tcp ok\r\n");
+
+jsonToSend="{\"Temperature\":";
+    dtostrf(sensor_tem,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+=",\"Humidity\":";
+    dtostrf(sensor_hum,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+=",\"voice\":";
+    dtostrf(sensor_voi,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+=",\"db\":";
+    dtostrf(db,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+     jsonToSend+=",\"PIR\":";
+    dtostrf(sensor_pir,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+="}";
+
+
+
+    postString="POST /devices/";
+    postString+=DEVICEID;
+    postString+="/datapoints?type=3 HTTP/1.1";
+    postString+="\r\n";
+    postString+="api-key:";
+    postString+=apiKey;
+    postString+="\r\n";
+    postString+="Host:api.heclouds.com\r\n";
+    postString+="Connection:close\r\n";
+    postString+="Content-Length:";
+    postString+=jsonToSend.length();
+    postString+="\r\n";
+    postString+="\r\n";
+    postString+=jsonToSend;
+    postString+="\r\n";
+    postString+="\r\n";
+    postString+="\r\n";
+
+  const char *postArray = postString.c_str();                 //将str转化为char数组
+  Serial.println(postArray);
+  wifi.send((const uint8_t*)postArray, strlen(postArray));    //send发送命令，参数必须是这两种格式，尤其是(const uint8_t*)
+  Serial.println("send success");   
+     if (wifi.releaseTCP()) {                                 //释放TCP连接
+        Serial.print("release tcp ok\r\n");
+        } 
+     else {
+        Serial.print("release tcp err\r\n");
+        }
+      postArray = NULL;                                       //清空数组，等待下次传输数据
+  
+  } else {
+    Serial.print("create tcp err\r\n");
+  }
+  
+}
